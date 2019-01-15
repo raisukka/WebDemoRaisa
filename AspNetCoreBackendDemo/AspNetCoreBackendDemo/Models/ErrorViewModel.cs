13a57234e4286416c0f9dd40e8327f58884e0f4b
Raisa Sukanen <raisa.sukanen@elisanet.fi>
2019-01-15 14:27:22 +0200

    Projektin luonti.

---
+++ AspNetCoreBackendDemo/AspNetCoreBackendDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreBackendDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
